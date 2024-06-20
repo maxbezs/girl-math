18a655110bc024e6f3930c8cbfd2d22082e9634d
maxbezs <baks.bezsmertnuy@gmail.com>
2024-06-20 16:12:12 +0200

    1

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,30 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "🌸 Girly Math 🌸",
   description:
     "Girl Math - app to track your girly expenses using girly app online for free",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }) {
+  async function serviceWorker() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((registration) => {
+          console.log(
+            "Service Worker registered with scope:",
+            registration.scope
+          );
+        })
+        .catch((error) => {
+          console.log("Service Worker registration failed:", error);
+        });
+    }
+  }
   return (
     <html lang="en">
       <head>
